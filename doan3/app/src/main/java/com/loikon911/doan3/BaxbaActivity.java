73fe190364243e5c17265ad12357d59e0f5c96f2
levanloi911 <54982847+levanloi911@users.noreply.github.com>
2019-09-12 11:43:49 +0700

    12/09

--- doan3/app/src/main/java/com/loikon911/doan3/BaxbaActivity.java
+++ doan3/app/src/main/java/com/loikon911/doan3/BaxbaActivity.java
@@ -10,183 +10,35 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AbsoluteLayout;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 
 public class BaxbaActivity extends AppCompatActivity {
-    Animation animation;
-    Button [][] buttons;
-    ImageView load;
-    boolean onclick;
-    int col, row, row1, col1;
+    private TextView moveCounter;
     MediaPlayer mediaPlayer;
-
-    //di chuyen
-    private void dichuyentrai(){
-        if (col>0){
-          String x= (String) buttons[row][ col -1].getText();
-          buttons[row][col].setText(x);
-          buttons[row] [col-1].setText("");
-          col = col -1;
-        }
-    }
-    private  void dichuyenphai(){
-        if (col<2) {
-            String x = (String) buttons[row][col + 1].getText();
-            buttons[row][col].setText(x);
-            buttons[row][col + 1].setText("");
-            col = col + 1;
-        }
-    }
-    private  void dichuyenlen(){
-        if (row>0) {
-            String x = (String) buttons[col][row -1 ].getText();
-            buttons[col][row].setText(x);
-            buttons[col][row -1 ].setText("");
-            row = row - 1;
-        }
-    }
-    private  void dichuyenxuong(){
-        if (row<2) {
-            String x = (String) buttons[col][row +1 ].getText();
-            buttons[col][row].setText(x);
-            buttons[col][row +1 ].setText("");
-            row = row + 1;
-        }
-    }
-    private void dichuyen(){
-        if (row == row1){
-            if (col == col1 -1) dichuyenphai();
-        }else if(col == col1 +1 ) dichuyentrai();
-        if (col==col1)
-        if(row1 == row +1)  dichuyenxuong();
-        else if(row1 == row -1)  dichuyenlen();
-    }
-
-    //1. Tạo một mảng gồm n phần tử các số ngẫu nhiên
-    private int[] taoMangNgauNhien(int n) {
-        int[] A = new int[n];
-        for (int i = 0; i < n; i++)
-            A[i] = i+1;
-        return tronNgauNhien(A);
-    }
-    private int[] tronNgauNhien(int[]A){
-        int n= A.length, j, tmp;
-        Random rd = new Random();
-        for(int i =0; i<n; i++)
-        {
-            j = rd.nextInt(n);
-            if(j<n) {
-                tmp=A[i];
-                A[i]=A[j];
-                A[j]=tmp;
-            }
-        }
-        return A;
-    }
-
+    private TextView feedbackText;
+    private Button[] buttons;
+    private Boolean bad_move=false;
+    private static final Integer[] goal = new Integer[] {0,1,2,3,4,5,6,7,8};
+    private ArrayList<Integer> cells = new ArrayList<Integer>();
+    ImageView load;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baxba);
-        Button start= (Button)findViewById(R.id.start);
         load = (ImageView)findViewById(R.id.loa);
         Toolbar toolbar = findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
-        //random and set text button
-        buttons = new Button[][]{{(Button)findViewById(R.id.bt1),(Button)findViewById(R.id.bt2),(Button)findViewById(R.id.bt3)},{(Button)findViewById(R.id.bt4),(Button)findViewById(R.id.bt5),(Button)findViewById(R.id.bt6)},{(Button)findViewById(R.id.bt7),(Button)findViewById(R.id.bt8),(Button)findViewById(R.id.bt9) }};
-
-
-        buttons[0][0].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row1 =0; col1 = 0;
-                dichuyen();
-            }
-        });
-        buttons[0][1].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row1 =0; col1 = 1;
-                dichuyen();
-            }
-        });
-        buttons[0][2].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row1 =0; col1 = 2;
-                dichuyen();
-            }
-        });
-        buttons[1][0].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row1 =1; col1 = 0;
-                dichuyen();
-            }
-        });
-        buttons[1][1].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row1 =1; col1 = 1;
-                dichuyen();
-            }
-        });
-        buttons[1][2].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row1 =1; col1 = 2;
-                dichuyen();
-            }
-        });
-        buttons[2][0].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row1 =2; col1 = 0;
-                dichuyen();
-            }
-        });
-        buttons[2][1].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row1 =2; col1 = 1;
-                dichuyen();
-            }
-        });
-        buttons[2][2].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                row1 =2; col1 = 2;
-                dichuyen();
-            }
-        });
-
-        Random random = new Random();
-        int[] x = taoMangNgauNhien(8);
-        for (int i = 0; i < 8; i++ )
-            if (i<3)
-        buttons[0][i%3].setText(x[i]+"");
-            else
-            if (i<6)
-                buttons[1][i%3].setText(x[i]+"");
-            else
-                buttons[2][i%3].setText(x[i]+"");
-
-        col =row=2;
-        checkwin();
-//        Intent intent = new Intent(Main2Activity.this,BaxbaActivity.class);
-//        Main2Activity.this.startActivity(intent);
-
-        Toast toast = Toast.makeText(BaxbaActivity.this, "sometext", Toast.LENGTH_LONG);
-        toast.show();
-
-//back home
+        //back home
         ActionBar actionbar = getSupportActionBar();
         if (actionbar != null) {
             actionbar.setDisplayHomeAsUpEnabled(true);
@@ -222,43 +74,207 @@ public class BaxbaActivity extends AppCompatActivity {
                 });
             }
         });
-        onclick = false;
-        if(onclick)
-        animation = AnimationUtils.loadAnimation(this,R.anim.remove);
-        else
-            animation = AnimationUtils.loadAnimation(this, R.anim.remove_right);
-        buttons[0][0].setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                v.setPadding(0,0,200,200);
+        buttons=findButtons();
 
-                return false;
-            }
-        });
+        for(int i=0;i<9;i++)
+        {
+            this.cells.add(i);
+        }
+        Collections.shuffle(this.cells); //random cells array
+
+        fill_grid();
 
 
 
+        moveCounter = (TextView) findViewById(R.id.MoveCounter);
+        feedbackText = (TextView) findViewById(R.id.FeedbackText);
+
+        for (int i = 1; i < 9; i++) {
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    makeMove((Button) v);
+                }
+            });
+        }
+
+
+        moveCounter.setText("0");
+        feedbackText.setText(R.string.game_feedback_text);
 
     }
-    private boolean checkwin(){
-        List<Byte> list = new ArrayList<>();
-        for (int i = 1; i<8; ++i){
-        list.add((byte) i);
-}
-        list.add(new Byte("0"));
+    public Button[] findButtons() {
+        Button[] b = new Button[9];
 
-        byte k = 0;
-        for (byte i = 0; i < 4; ++i) {
-            for (byte j = 0; j < 4; ++j) {
-                if (!this.buttons[i][j].getText().equals(list.get(k).toString())) {
-                    return false;
-                }
-                ++k;
+        b[0] = (Button) findViewById(R.id.Button00);
+        b[1] = (Button) findViewById(R.id.Button01);
+        b[2] = (Button) findViewById(R.id.Button02);
+        b[3] = (Button) findViewById(R.id.Button03);
+        b[4] = (Button) findViewById(R.id.Button04);
+        b[5] = (Button) findViewById(R.id.Button05);
+        b[6] = (Button) findViewById(R.id.Button06);
+        b[7] = (Button) findViewById(R.id.Button07);
+        b[8] = (Button) findViewById(R.id.Button08);
+        return b;
+    }
+
+    public void makeMove(final Button b) {
+        bad_move=true;
+        int b_text,b_pos,zuk_pos;
+        b_text=Integer.parseInt((String) b.getText());
+        b_pos=find_pos(b_text);
+        zuk_pos=find_pos(0);
+        switch(zuk_pos)
+        {
+            case(0):
+                if(b_pos==1||b_pos==3)
+                    bad_move=false;
+                break;
+            case(1):
+                if(b_pos==0||b_pos==2||b_pos==4)
+                    bad_move=false;
+                break;
+            case(2):
+                if(b_pos==1||b_pos==5)
+                    bad_move=false;
+                break;
+            case(3):
+                if(b_pos==0||b_pos==4||b_pos==6)
+                    bad_move=false;
+                break;
+            case(4):
+                if(b_pos==1||b_pos==3||b_pos==5||b_pos==7)
+                    bad_move=false;
+                break;
+            case(5):
+                if(b_pos==2||b_pos==4||b_pos==8)
+                    bad_move=false;
+                break;
+            case(6):
+                if(b_pos==3||b_pos==7)
+                    bad_move=false;
+                break;
+            case(7):
+                if(b_pos==4||b_pos==6||b_pos==8)
+                    bad_move=false;
+                break;
+            case(8):
+                if(b_pos==5||b_pos==7)
+                    bad_move=false;
+                break;
+        }
+
+        if(bad_move==true)
+        {
+            feedbackText.setText("Move Not Allowed");
+            return;
+        }
+        feedbackText.setText("Move OK");
+        cells.remove(b_pos);
+        cells.add(b_pos, 0);
+        cells.remove(zuk_pos);
+        cells.add(zuk_pos,b_text);
+
+
+        fill_grid();
+        moveCounter.setText(Integer.toString(Integer.parseInt((String) moveCounter.getText())+1));
+
+        //check win
+        for(int i=0;i<9;i++)
+        {
+            if(cells.get(i)!=goal[i])
+            {
+                return;
             }
         }
-        return true;
+        feedbackText.setText("we have a winner");
     }
 
+    public void fill_grid() {
+        for (int i = 0; i < 9; i++) {
+            int text = cells.get(i);
+            AbsoluteLayout.LayoutParams absParams = (AbsoluteLayout.LayoutParams) buttons[text].getLayoutParams();
+            switch (i)
+            //Này là logic code mà.. kiểm tra toạ độ x,y của một cái view
+            //Rồi set lại layout cho cái view đó
+
+            {
+                case (0):
+
+                    absParams.x = 5;
+                    absParams.y = 5;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (1):
+                    absParams.x = 110;
+                    absParams.y = 5;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (2):
+
+                    absParams.x = 215;
+                    absParams.y = 5;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (3):
+
+                    absParams.x = 5;
+                    absParams.y = 110;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (4):
+
+                    absParams.x = 110;
+                    absParams.y = 110;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (5):
+
+                    absParams.x = 215;
+                    absParams.y = 110;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (6):
+
+                    absParams.x = 5;
+                    absParams.y = 215;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (7):
+
+                    absParams.x = 110;
+                    absParams.y = 215;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+                case (8):
+
+                    absParams.x = 215;
+                    absParams.y = 215;
+                    buttons[text].setLayoutParams(absParams);
+                    break;
+
+
+            }
+
+        }
+    }
+
+    public int find_pos(int element)
+    {
+        int i=0;
+        for(i=0;i<9;i++)
+        {
+            if(cells.get(i)==element)
+            {
+                break;
+            }
+        }
+        return i;
+    }
 }
+
+
+
+
+
 
 
